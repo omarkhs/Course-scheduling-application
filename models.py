d45035268f5bfb42ffc257b5580fe97f4f722315
omarkhs <omarkhsalem@gmail.com>
2018-06-20 23:36:11 -0700

    Created new Course class

--- models.py
+++ models.py
@@ -1,1 +1,10 @@
 # Course model
+class Course():
+    """ a class for courses """
+
+# TODO: add rest of attributes
+    def __init__(self, name, title, code_number):
+        """ Initialize Course and its attributes"""
+        self.name = name
+        self.title = title
+        self.code_number = code_number
